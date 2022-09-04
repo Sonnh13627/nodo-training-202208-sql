b440e7d45b874e0ec4d884ed754cab6e2c07827e
Sonnh13627 <hongson25082002@gmail.com>
2022-09-05 03:02:11 +0700

    upcode

---
+++ Spring-Security-Hibernate/src/test/java/com/example/springsecurityhibernate/SpringSecurityHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
